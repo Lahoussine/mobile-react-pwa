bc74e8a832268f145a4380ba6c64130687acd47a
lahoussine.derradji@gmail.com <lahoussine.derradji@gmail.com>
2020-02-25 19:34:18 +0100

    Updates

---
+++ precache-manifest.cf75ad47ea119c4ab6cc21d912146ecc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909dd71ed330cd2af52c6ce48d1605d5",
+    "url": "/mobile-react-pwa/index.html"
+  },
+  {
+    "revision": "4f9856755a2b1af20cd9",
+    "url": "/mobile-react-pwa/static/css/main.eee32b30.chunk.css"
+  },
+  {
+    "revision": "a7917f27d20b3214169d",
+    "url": "/mobile-react-pwa/static/js/2.af21fa59.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mobile-react-pwa/static/js/2.af21fa59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9856755a2b1af20cd9",
+    "url": "/mobile-react-pwa/static/js/main.1ebb18bf.chunk.js"
+  },
+  {
+    "revision": "dc0a7eedf559dad2bef5",
+    "url": "/mobile-react-pwa/static/js/runtime-main.5e94bfaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mobile-react-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
